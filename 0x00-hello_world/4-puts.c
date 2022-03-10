8c738967cb06dd49081bc8d6de8934ecc0bb1793
root <you@example.com>
2022-03-10 13:49:30 -0800

    low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,8 @@
 
 
 /**
- *
- *  * main - prints the phrase "Programming is like building a multilingual puzzle
- *
- *   * Return: 0 if exited properly, non-zero otherwise
+ *main - prints	the phrase "Programming is like building a multilingual puzzle
+ *    Return: 0 if exited properly, non-zero otherwise
  *
  *    */
 
