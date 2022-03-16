c69ca18334864954ec7c233e06a9a69bdfd42c43
root <you@example.com>
2022-03-16 12:19:36 -0700

    loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,9 @@
-#header file containing declarations for all functions used 
-1. int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * file: main.h
+ * desc: declarartions for files used
+ */
+
+int _putchar(char c);
