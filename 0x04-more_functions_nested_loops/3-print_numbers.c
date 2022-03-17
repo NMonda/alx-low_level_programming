4c9a1e90b04f6a93786143f7aeb82045d6be9b4a
root <you@example.com>
2022-03-17 13:38:29 -0700

    nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,12 @@
  * followed by a new line
  * Return: Always 0 (Success)
  */
+
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
 	}
