27ea106c5aa48a2d495750d32ca3b39340e3dcb2
root <you@example.com>
2022-03-21 19:08:08 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 updates value
+ * pointer is 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+1. Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
+2. 
